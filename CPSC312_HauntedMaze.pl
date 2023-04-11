da646ded80365337d0bf06a0ca8ed9236e4e539d
kman464 <kman464@students.cs.ubc.ca>
2023-04-10 19:20:28 -0700

    Rewrote the majority of the code so that it would function as intended. No new features added.

--- CPSC312_HauntedMaze.pl
+++ CPSC312_HauntedMaze.pl
@@ -4,18 +4,18 @@ room(grand_hallway, 'Grand Hallway', 'You are in the grand hallway. Large portra
 room(kitchen, 'Kitchen', 'You are in the kitchen. The smell of rotten food fills your nostrils, it makes you sick...').
 room(library, 'Library', 'You are among many books in the library...').
 room(staircase, 'Staircase', 'You stand at the bottom of a large staircase. You can either take them up or down...').
-room(front_entrance, 'Front Entrance', 'You are in the front entrance. The old wooden door behind you doesn\'t do much to keep out the howling wind...').
-room(pool, 'Pool', 'You stand by the pool. So much grime has built on the surface you can\'t see what lurks below...').
+room(front_entrance, 'Front Entrance', 'You are in the front entrance. The old wooden door behind you doesn''t do much to keep out the howling wind...').
+room(pool, 'Pool', 'You stand by the pool. So much grime has built on the surface you can''t see what lurks below...').
 room(dining_hall, 'Dining Hall', 'You are in the dining hall. A large rectangular table streches the length of the room, there was once many people who gathered here...').
 
 %Basement 
 room(boiler_room, 'Boiler Room', 'You have found the boiler room. The low rumble of the boiler sounds like the growl of a beast...').
 room(basement_landing, 'Basement Landing', 'You are in the basement landing. The light from the top of the stairs reveals the dusty floor littered with mouse droppings...').
-room(servants_quarters, 'Servants ', 'You are in the servants quarters. There are many personal beliongings strewn about, looks like they left in a hurry...').
+room(servants_quarters, 'Servants Quarters', 'You are in the servants quarters. There are many personal beliongings strewn about, looks like they left in a hurry...').
 
 %Upper Floor
 room(mezzanine, 'Mezzanine', 'You stand at the railing of the mezzanine looking over the grand hall. You swear there are less suits of armor than you remember...').
-room(master_bedroom, 'master_bedroom', 'You are in the master bedroom. There are large claw marks through the center of the bed, you hope there was no one sleeping there when that happend...').
+room(master_bedroom, 'Master Bedroom', 'You are in the master bedroom. There are large claw marks through the center of the bed, you hope there was no one sleeping there when that happend...').
 room(balcony, 'Balcony', 'You are on the balcony. There is a trellis that runs down the side of the manor to the garden, maybe you could climb down...').
 room(guest_bedroom, 'Guest Bedroom', 'You are in the guest bedroom. There are brokem chains that are hooked to the wall, was someone kept prisoner here?').
 
@@ -71,65 +71,52 @@ connected(east, balcony, master_bedroom).
 connected(down, balcony, garden).
 connected(up, garden, balcony).
 
-%Predicate to display room player is currently in
+
+
+
+% Define the starting room for the player
 :- dynamic current_room/1.
 
+
+% Define a predicate to print the current location
 print_location :-
     current_room(Current),
     room(Current, Name, Description),
-    print(Name), nl, print(Description), nl.
+    write(Name), nl,
+    write(Description), nl.
 
-%Predicate to change the current room
+% Define a predicate to change the current room
 change_room(NewRoom) :-
-    current_room(Current),
-    retract(current_room(Current)),
+    retract(current_room(_)),
     assertz(current_room(NewRoom)).
 
-%Predicates to take user input using readln library
-:- use_module(library(readln)).
+% Define a predicate to handle user input
+process_input([quit]) :-
+    write('Exiting game...'), false.
 
-%Predicate to make the console prompt the player
-:- prompt(_, 'Type a command (or ''help''): ').
-
-%Predicate to strip punctuation from user input
-strip_punctuation([], []).
-
-strip_punctuation([Word|Tail], [Word|Result]) :-
-    \+(member(Word, ['.', ',', '?', '!'])),
-    strip_punctuation(Tail, Result).
-
-strip_punctuation([_|Tail], Result) :-
-    strip_punctuation(Tail, Result).
-
-%Predicate to read the users input with readln
-read_sentence(Input) :-
-    readln(Input1, _, ".!?", "_0123456789", lowercase),
-    strip_punctuation(Input1, Input).
-
-%Predicate to take input from the console and process it
-get_input :- read_sentence(Input), get_input(Input).
-get_input([quit]).
-get_input(Input) :-
-    process_input(Input), print_location,
-    read_sentence(Input1), get_input(Input1).
-	
-%Predicate to parse user input
-process_input([help]) :- print('Type one of the 8 directions on a compass to try to move to a connected room'), nl.
+process_input([help]) :-
+    write('Type one of the 4 directions (north, south, east, west) to move to a connected room.'), nl, nl, !.
 
 process_input([go, Direction]) :-
     current_room(Current),
     connected(Direction, Current, NewRoom),
-    change_room(NewRoom).
+    change_room(NewRoom),
+    write('You have moved to the '), write(NewRoom), nl, nl.
 
-process_input([go, _]) :-
-    print('No exit that direction.'), nl.
+process_input([]) :-
+    write('Huh?'), nl, nl.
 
-process_input([_]) :-
-    print('Huh?'), nl, nl.
-	
-%Main Predicate to play the game
+% Define a predicate to read user input and process it
+get_input :-
+    write('Type a command (or "help"): '),
+    readln(Input),
+    process_input(Input),
+    print_location,
+    get_input.
+
+% Define a predicate to start the game
 play :-
-    retractall(current_room(_)),
-    assertz(current_room(library)),
+	retractall(current_room(_)),
+	assertz(current_room(front_entrance)),
     print_location,
     get_input.
